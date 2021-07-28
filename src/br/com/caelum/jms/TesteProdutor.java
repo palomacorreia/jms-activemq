c9d777126b34dca4769d840a7fd4e4ea0fada55f
Paloma <paloma.a.silva@capgemini.com>
2021-07-27 21:37:12 -0300

    Produtor

---
+++ src/br/com/caelum/jms/TesteProdutor.java
@@ -0,0 +1,34 @@
+package br.com.caelum.jms;
+
+import javax.jms.*;
+import javax.naming.InitialContext;
+import java.util.Scanner;
+
+public class TesteProdutor {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws Exception {
+		
+		InitialContext context = new InitialContext();
+		ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+		
+		Connection connection = factory.createConnection(); 
+		connection.start();
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		
+		Destination fila = (Destination) context.lookup("financeiro");
+
+		MessageProducer producer = session.createProducer(fila);
+
+		for(int i = 0; i < 1000; i ++) {
+			Message message = session.createTextMessage("<pedido><id>" + i + "</id></pedido>");
+			producer.send(message);
+		}
+
+		new Scanner(System.in).nextLine();
+		
+		session.close();
+		connection.close();
+		context.close();
+	}
+}
